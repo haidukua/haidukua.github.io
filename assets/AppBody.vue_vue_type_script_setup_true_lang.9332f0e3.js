5e36b584f2c0169a558a0e019262d1e2345a8696
haidukua <haidukua@gmail.com>
2022-10-18 15:49:57 +0300

    update

---
+++ assets/AppBody.vue_vue_type_script_setup_true_lang.9332f0e3.js
@@ -0,0 +1,1 @@
+import{d as s,f as t,j as n,c,o as e,k as d}from"./index.255d102b.js";const r={class:"a_body"},p=s({__name:"AppBody",props:{isLoaded:{type:Boolean,default:!0}},setup(o){return(a,_)=>(e(),t("div",r,[o.isLoaded?n(a.$slots,"default",{key:0}):(e(),c(d,{key:1}))]))}});export{p as _};
